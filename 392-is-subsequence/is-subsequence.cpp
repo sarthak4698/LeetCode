6dffdddfe9f17d7d2c47aae0928dfae6787b7c8b
sarthak4698 <98869592+sarthak4698@users.noreply.github.com>
2024-10-21 08:03:03 +0530

    Time: 0 ms (100.00%) | Memory: 7.8 MB (68.27%) - LeetSync

--- 392-is-subsequence/is-subsequence.cpp
+++ 392-is-subsequence/is-subsequence.cpp
@@ -3,25 +3,26 @@ public:
     bool isSubsequence(string s, string t) {
         if(s.empty())
         {
-            return true;
+            return true ;
+
         }
-        if(s==t)
+        if(s ==t )
         {
-            return true;
+            return true ;
+
         }
         int j =0;
         for(int i =0 ; i<t.size(); i++)
         {
             if(s[j]==t[i])
             {
-                j++;
-                if(j ==s.size())
-                {
+               j++;
+               if(j == s.size())
+               {
                     return true;
-                }
+               }
             }
         }
-
         return j == s.size();
     }
 };
