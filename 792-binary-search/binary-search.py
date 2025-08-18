f7eb18afe9ba4af561b485c1bc4bd428727f0da0
sarthak4698 <98869592+sarthak4698@users.noreply.github.com>
2025-08-18 21:42:00 +0530

    Time: 0 ms (100.00%) | Memory: 13.3 MB (92.89%) - LeetSync

---
+++ 792-binary-search/binary-search.py
@@ -0,0 +1,10 @@
+class Solution(object):
+    def search(self, nums, target):
+
+        n = len(nums)
+
+        for i in range (n):
+            if nums[i]==target:
+                return i
+
+        return -1
