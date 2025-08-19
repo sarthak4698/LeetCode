48a559374c4e60c75cd9096e98d04fe67ac70595
sarthak4698 <98869592+sarthak4698@users.noreply.github.com>
2025-08-19 14:05:56 +0530

    Time: 0 ms (100.00%) | Memory: 12.9 MB (32.04%) - LeetSync

---
+++ 81-search-in-rotated-sorted-array-ii/search-in-rotated-sorted-array-ii.py
@@ -0,0 +1,14 @@
+class Solution(object):
+    def search(self, nums, target):
+        """
+        :type nums: List[int]
+        :type target: int
+        :rtype: bool
+        """
+        n = len(nums)
+
+        for i in range(n):
+            if nums[i]==target:
+                return True
+        
+        return False
