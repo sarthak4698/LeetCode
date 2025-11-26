467d279f2a3798fcc06d591757845cb0cc226614
sarthak4698 <98869592+sarthak4698@users.noreply.github.com>
2025-11-26 23:50:52 +0530

    Time: 3013 ms (5.03%) | Memory: 13.2 MB (99.14%) - LeetSync

--- 268-missing-number/missing-number.py
+++ 268-missing-number/missing-number.py
@@ -1,11 +1,10 @@
 class Solution(object):
     def missingNumber(self, nums):
-        
-        cum_sum =0
         n = len(nums)
-
         for i in range(n):
-            cum_sum +=nums[i]
-            exp_sum = n*(n+1)//2
-            result = exp_sum - cum_sum
-        return result
+
+            expected = n * (n+1)//2
+            actual = sum(nums)
+            missing = expected - actual
+        
+        return missing 
